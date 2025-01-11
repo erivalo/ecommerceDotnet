17e97a449fa29512086fcab27c19c3c7902386c9
max-pw <maxpowerxp00@gmail.com>
2025-01-11 07:48:54 -0400

    Add shared project to remove duplicated code

---
+++ basket-microservice/shared-libs/ECommerce.Shared/Infrastructure/RabbitMq/RabbitMqOptions.cs
@@ -0,0 +1,6 @@
+namespace ECommerce.Shared.Infrastructure.RabbitMq;
+public class RabbitMqOptions
+{
+  public const string RabbitMqSectionName = "RabbitMq";
+  public string HostName { get; set; } = string.Empty;
+}
