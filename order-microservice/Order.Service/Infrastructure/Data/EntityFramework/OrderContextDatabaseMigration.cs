09fe0ada439a6cfd265d1bb4aa8fdf7b65f695e9
erivalo <erivalo007@gmail.com>
2025-04-13 21:31:00 -0400

    Add order trace

---
+++ order-microservice/Order.Service/Infrastructure/Data/EntityFramework/OrderContextDatabaseMigration.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Order.Service.Infrastructure.Data.EntityFramework;
+internal static class OrderContextDatabaseMigration
+{
+  public static void MigrateDatabase(this WebApplication webApp)
+  {
+    using var scope = webApp.Services.CreateScope();
+    using var orderContext = scope.ServiceProvider.GetRequiredService<OrderContext>();
+    orderContext.Database.Migrate();
+  }
+}
--- order-microservice/Order.Service/Program.cs
+++ order-microservice/Order.Service/Program.cs
@@ -1,7 +1,7 @@
-using Order.Service.Infrastructure.Data;
 using Order.Service.Endpoints;
 using ECommerce.Shared.Infrastructure.RabbitMq;
 using Order.Service.Infrastructure.Data.EntityFramework;
+using ECommerce.Shared.Observability;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,8 +14,17 @@ builder.Services.AddSqlServerDataStore(builder.Configuration);
 builder.Services
   .AddRabbitMqEventBus(builder.Configuration)
   .AddRabbitMqEventPublisher();
+builder.Services.AddOpenTelemetryTracing(
+  "Order",
+  builder.Configuration,
+  traceBuilder => traceBuilder.WithSqlInstrumentation());
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+  app.MigrateDatabase();
+}
 
 app.RegisterEndpoints();
 
