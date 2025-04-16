29178ac1f3e7573358f84f652c132a5a1a0a9384
max-pw <maxpowerxp00@gmail.com>
2025-04-16 12:00:59 -0400

    Add sql retry resilience strategy

--- product-microservice/Product.Service/Infrastructure/Data/EntityFramework/EntityFrameworkExtensions.cs
+++ product-microservice/Product.Service/Infrastructure/Data/EntityFramework/EntityFrameworkExtensions.cs
@@ -7,7 +7,16 @@ public static class EntityFrameworkExtensions
     IConfigurationManager configuration)
   {
     services.AddDbContext<ProductContext>(options =>
-      options.UseSqlServer(configuration.GetConnectionString("Default")));
+      options.UseSqlServer(
+        configuration.GetConnectionString("Default"),
+        sqlServerOptionsAction: sqlOptions =>
+        {
+          sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(40),
+            errorNumbersToAdd: [0]);
+        }
+        ));
 
     services.AddScoped<IProductStore, ProductContext>();
   }
