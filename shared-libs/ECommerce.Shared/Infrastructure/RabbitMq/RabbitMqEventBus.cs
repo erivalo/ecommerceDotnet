e6e61409cd207449314fb3ee305f284d770b693e
max-pw <maxpowerxp00@gmail.com>
2025-04-16 13:15:02 -0400

    Add resilient approach for product with polly

--- shared-libs/ECommerce.Shared/Infrastructure/RabbitMq/RabbitMqEventBus.cs
+++ shared-libs/ECommerce.Shared/Infrastructure/RabbitMq/RabbitMqEventBus.cs
@@ -1,11 +1,16 @@
 
 using System.Diagnostics;
+using System.Net.Sockets;
 using System.Text.Json;
 using ECommerce.Shared.Infrastructure.EventBus;
 using ECommerce.Shared.Infrastructure.EventBus.Abstractions;
 using ECommerce.Shared.Observability;
+using Microsoft.Extensions.Options;
 using OpenTelemetry;
 using OpenTelemetry.Context.Propagation;
+using Polly;
+using Polly.Retry;
+using RabbitMQ.Client.Exceptions;
 
 namespace ECommerce.Shared.Infrastructure.RabbitMq;
 public class RabbitMqEventBus : IEventBus
@@ -15,52 +20,60 @@ public class RabbitMqEventBus : IEventBus
   private readonly IRabbitMqConnection _rabbitMqConnection;
   private readonly ActivitySource _activitySource;
   private readonly TextMapPropagator _propagator = Propagators.DefaultTextMapPropagator;
+  private readonly ResiliencePipeline _pipeline;
 
-  public RabbitMqEventBus(IRabbitMqConnection rabbitMqConnection, RabbitMqTelemetry rabbitMqTelemetry)
+  public RabbitMqEventBus(
+    IRabbitMqConnection rabbitMqConnection,
+    RabbitMqTelemetry rabbitMqTelemetry,
+    IOptions<EventBusOptions> options)
   {
     _rabbitMqConnection = rabbitMqConnection;
     _activitySource = rabbitMqTelemetry.ActivitySource;
+    _pipeline = CreateResiliencePipeline(options.Value.RetryCount);
   }
 
   public Task PublishAsync(Event @event)
   {
     var routingKey = @event.GetType().Name;
-
-    using var channel = _rabbitMqConnection.Connection.CreateModel();
-
-    var activityName = $"{OpenTelemetryMessagingConventions.PublishOperation} {routingKey}";
-    using var activity = _activitySource.StartActivity(activityName, ActivityKind.Client);
-    ActivityContext activityContextToInject = default;
-    if (activity is not null)
+    return _pipeline.Execute(() =>
     {
-      activityContextToInject = activity.Context;
-    }
-    var properties = channel.CreateBasicProperties();
-    _propagator.Inject(new PropagationContext(activityContextToInject, Baggage.Current), properties,
-      (properties, key, value) =>
-        {
-          properties.Headers ??= new Dictionary<string, object>();
-          properties.Headers[key] = value;
-        });
-    SetActivityContext(activity, routingKey, OpenTelemetryMessagingConventions.PublishOperation);
+      using var channel = _rabbitMqConnection.Connection.CreateModel();
 
-    channel.ExchangeDeclare(
-        exchange: ExchangeName,
-        type: "fanout",
-        durable: false,
-        autoDelete: false,
-        null);
+      var activityName = $"{OpenTelemetryMessagingConventions.PublishOperation} {routingKey}";
+      using var activity = _activitySource.StartActivity(activityName, ActivityKind.Client);
+      ActivityContext activityContextToInject = default;
+      if (activity is not null)
+      {
+        activityContextToInject = activity.Context;
+      }
+      var properties = channel.CreateBasicProperties();
+      _propagator.Inject(new PropagationContext(activityContextToInject, Baggage.Current), properties,
+        (properties, key, value) =>
+          {
+            properties.Headers ??= new Dictionary<string, object>();
+            properties.Headers[key] = value;
+          });
+      SetActivityContext(activity, routingKey, OpenTelemetryMessagingConventions.PublishOperation);
 
-    var body = JsonSerializer.SerializeToUtf8Bytes(@event, @event.GetType());
+      channel.ExchangeDeclare(
+          exchange: ExchangeName,
+          type: "fanout",
+          durable: false,
+          autoDelete: false,
+          null);
 
-    channel.BasicPublish(
-        exchange: ExchangeName,
-        routingKey: routingKey,
-        mandatory: false,
-        basicProperties: properties,
-        body: body);
+      var body = JsonSerializer.SerializeToUtf8Bytes(@event, @event.GetType());
 
-    return Task.CompletedTask;
+      channel.BasicPublish(
+          exchange: ExchangeName,
+          routingKey: routingKey,
+          mandatory: false,
+          basicProperties: properties,
+          body: body);
+
+      return Task.CompletedTask;
+
+    });
   }
   private static void SetActivityContext(Activity? activity, string routingKey, string operation)
   {
@@ -70,5 +83,20 @@ public class RabbitMqEventBus : IEventBus
       activity.SetTag(OpenTelemetryMessagingConventions.OperationName, operation);
       activity.SetTag(OpenTelemetryMessagingConventions.DestinationName, routingKey);
     }
+  }
+  private static ResiliencePipeline CreateResiliencePipeline(int retryCount)
+  {
+    var retryOptions = new RetryStrategyOptions
+    {
+      ShouldHandle = new PredicateBuilder()
+        .Handle<BrokerUnreachableException>()
+        .Handle<SocketException>()
+        .Handle<AlreadyClosedException>(),
+      BackoffType = DelayBackoffType.Exponential,
+      MaxRetryAttempts = retryCount,
+    };
+    return new ResiliencePipelineBuilder()
+      .AddRetry(retryOptions)
+      .Build();
   }
 }
